a1bbaf0a8248cc19ea531b6848273643f028f649
StephanieFrias <stephanie.frias@gmail.com>
2013-07-17 21:50:00 -0400

    BSS_SMS_QUoteBot

--- local_settings.py
+++ local_settings.py
@@ -8,7 +8,7 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 ACCOUNT_SID = "AC9bda99a6330ad8c75de80fddac19e7a7" 
 AUTH_TOKEN = "b4f942e6b561270b682ff5aa73f351a9"
 BSSSPAM_APP_SID = "AP9608d4a6e1007caf24801e71f5f6e3f5"
-BSS_SPAM_ID = "+13056005508"
+BSS_SPAM_ID = "+1305605508"
 
 
 # Begin Heroku configuration - configured through environment variables.
